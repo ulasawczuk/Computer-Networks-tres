4054633c839831c94a826edc110c9626eccb5c7e
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-09-29 13:49:50 +0000

    Update puzzleSolver.cpp

--- puzzleSolver.cpp
+++ puzzleSolver.cpp
@@ -14,13 +14,13 @@
 #include <iomanip>
 
 #define BUFFER_SIZE 1024
-#define RETRY_COUNT 3
-#define GROUP_ID 64
+#define RETRY_COUNT 3 // times we're going to try to send the message
+#define GROUP_ID 113
 
 #define TIMEOUT_SEC 0
 #define TIMEOUT_USEC 50000 // 0.5 seconds in microseconds
 
-// IP header from slides
+// IP structure from slides
 struct ip
 {
 #if BYTE_ORDER == LITTLE_ENDIAN
@@ -45,6 +45,7 @@ struct ip
     struct in_addr ip_src, ip_dst; /* source and dest address */
 };
 
+// iphdr structure
 struct iphdr
 {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
@@ -68,7 +69,7 @@ struct iphdr
     /*The options start here. */
 };
 
-// UDP header struct
+// UDP header structure
 struct updhdr
 {
     unsigned short source;
@@ -77,6 +78,7 @@ struct updhdr
     unsigned short check;
 };
 
+// pseudo header structure
 struct pseudo_header
 {
     u_int32_t source_address;
@@ -84,6 +86,28 @@ struct pseudo_header
     u_int8_t placeholder;
     u_int8_t protocol;
     u_int16_t udp_length;
+};
+
+// icmphdr structure, not used in the end
+struct icmphdr
+{
+  u_int8_t type;                /* message type */
+  u_int8_t code;                /* type sub-code */
+  u_int16_t checksum;
+  union
+  {
+    struct
+    {
+      u_int16_t        id;
+      u_int16_t        sequence;
+    } echo;                        /* echo datagram */
+    u_int32_t        gateway;        /* gateway address */
+    struct
+    {
+      u_int16_t        __unused;
+      u_int16_t        mtu;
+    } frag;                        /* path mtu discovery */
+  } un;
 };
 
 /*
@@ -96,8 +120,8 @@ Server response: Well done group 64. You have earned the right to know the port:
 // method for getting the signature
 uint32_t getSignature()
 {
-    uint32_t secret = 0x9eedfeaf;
-    uint32_t challenge = 0xbea9f44d;
+    uint32_t secret = 0xafdccf9e;
+    uint32_t challenge = 0xbdbae77c;
 
     // xor operation
     uint32_t signed_challenge = secret ^ challenge;
@@ -146,18 +170,13 @@ uint16_t udp_checksum(struct updhdr *p_udp_header, size_t len, uint32_t src_addr
 
 char* getKnock(std::string secret_phrase) {
 
+    // get signature
     uint32_t signature = getSignature();
-    //std::stringstream ss;
-    //ss << std::hex << std::setw(8) << std::setfill('0') << signature;  // Format to 8 hex digits
-    //std::string hex_signature = ss.str();
 
     size_t message_length = 4 + secret_phrase.length();
-    //size_t message_length = hex_signature.length() + secret_phrase.length();
     char* message = new char[message_length];
 
-    //memcpy(message, hex_signature.c_str(), hex_signature.length());
-    //memcpy(message + hex_signature.length(), secret_phrase.c_str(), secret_phrase.length());
-
+    // combine signature and secret phrase
     memcpy(message, &signature, sizeof(signature));
     memcpy(message + 4, secret_phrase.c_str(), secret_phrase.length());
 
@@ -169,7 +188,7 @@ char* getKnock(std::string secret_phrase) {
 // method for the secret port
 int secretPort(int sock, sockaddr_in server_addr, int port)
 {
-    unsigned char group_id = GROUP_ID; 
+    unsigned char group_id = GROUP_ID; // sorting group ID
 
     for (int attempt = 0; attempt < RETRY_COUNT; ++attempt)
     {
@@ -177,7 +196,7 @@ int secretPort(int sock, sockaddr_in server_addr, int port)
         if (sendto(sock, &group_id, sizeof(group_id), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
             std::cerr << "Failed to send packet to port " << port << ": " << strerror(errno) << std::endl;
-            return -1;
+            continue;
             ;
         }
 
@@ -227,10 +246,10 @@ int secretPort(int sock, sockaddr_in server_addr, int port)
         if (sendto(sock, message, sizeof(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
             std::cerr << "Failed to send packet to port " << port << ": " << strerror(errno) << std::endl;
-            return -1;
+            continue;
         }
 
-        memset(buffer, 0, sizeof(buffer));
+        memset(buffer, 0, sizeof(buffer)); // clearing the buffer
         memset(buffer, 0, sizeof(buffer));
 
         recv_len = recvfrom(sock, buffer, BUFFER_SIZE - 1, 0, NULL, NULL);
@@ -241,12 +260,11 @@ int secretPort(int sock, sockaddr_in server_addr, int port)
         }
         else
         {
-            std::cerr << "Failed to receive response: " << port << std::endl;
+            std::cerr << "recvfrom failed: " << strerror(errno) << std::endl;
             continue;
         }
 
-        // get the secret port from response
-
+        // extract the secret port from response
         std::cout << "Extracting secret port...." << std::endl;
 
         char port_str[5];
@@ -286,7 +304,7 @@ int sendSignatureEvil(int sock, sockaddr_in server_addr, int port, const char *t
     }
     printf("OK: socket option IP_HDRINCL is set.\n");
 
-    // set timeout
+    // set timeout of 5 secs
     struct timeval timeout;
     timeout.tv_sec = 5;
     timeout.tv_usec = 0;
@@ -306,14 +324,14 @@ int sendSignatureEvil(int sock, sockaddr_in server_addr, int port, const char *t
     iph->version = 4;
     iph->tos = 0;
     iph->tot_len = sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(message);
-    iph->id = htonl(54321); // Id of this packet
-    iph->frag_off = 0x8000;
+    iph->id = htonl(54321); 
+    iph->frag_off = 0x8000; // the evil bit!!!
 #ifdef __linux__
     iph->frag_off = htons(0x8000);
 #endif
     iph->ttl = 255;
     iph->protocol = IPPROTO_UDP;
-    iph->check = 0; // Set to 0 before calculating checksum
+    iph->check = 0;
 
     // std::cerr << "Size of struct IP: " << sizeof(struct iphdr) << std::endl;
     // std::cerr << "Size of struct udphdr: " << sizeof(struct udphdr) << std::endl;
@@ -352,13 +370,14 @@ int sendSignatureEvil(int sock, sockaddr_in server_addr, int port, const char *t
 
     memcpy(buffer + sizeof(struct ip) + sizeof(struct udphdr), &message, sizeof(message));
 
+    // send the packet with the evil bit
     for (int attempt = 0; attempt < RETRY_COUNT; ++attempt)
     {
         std::cout << "Sending packet with evil bit...." << std::endl;
-        // send the packet
         if (sendto(sd, buffer, iph->tot_len, 0, (struct sockaddr *)&sin, sizeof(sin)) < 0)
         {
             perror("sendto failed");
+            continue;
         }
         else
         {
@@ -386,7 +405,7 @@ int sendSignatureEvil(int sock, sockaddr_in server_addr, int port, const char *t
             continue;
         }
 
-        // get the secret port
+        // extract the secret port
         std::cout << "Extracting secret port...." << std::endl;
         char port_str[5]; 
         
@@ -416,7 +435,7 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
         if (sendto(sock, &message, sizeof(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
             std::cerr << "Failed to send packet to port " << port << ": " << strerror(errno) << std::endl;
-            return "";
+            continue;
         }
         else
         {
@@ -435,6 +454,7 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
         }
         else
         {
+            std::cerr << "recvfrom failed: " << strerror(errno) << std::endl;
             continue;
         }
 
@@ -507,12 +527,11 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
             std::cerr << "Calculated checksum: " << calculated_checksum << ", added payload: " << data << std::endl;
             udp_hd->check = calculated_checksum;
             uint16_t checksum_in_header = ntohs(udp_hd->check);
-            // std::cerr << "Checksum in UDP header: " << std::hex << checksum_in_header << std::endl;
             break;
         }
     }
 
-    // sending the packet
+    // sending the packet with correct checksum
     for (int attempt = 0; attempt < RETRY_COUNT; ++attempt)
     {
         std::cout << "Sending packet with calculated checksum...." << std::endl;
@@ -520,6 +539,7 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
         if (sent_len < 0)
         {
             std::cerr << "Failed to send packet: " << strerror(errno) << std::endl;
+            continue;
         }
         else
         {
@@ -542,19 +562,18 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
             continue;
         }
 
-        // find the secret phrase
-
+        // extract the secret phrase by finding quotation marks
         std::cout << "Extracting secret phrase...." << std::endl;
         char *start = strchr(buffer, '"');
         if (start == nullptr) {
             std::cerr << "Failed to find the starting quotation mark." << std::endl;
-            return "";
+            continue;
         }
         
         char *end = strchr(start + 1, '"');
         if (end == nullptr) {
             std::cerr << "Failed to find the ending quotation mark." << std::endl;
-            return "";
+            continue;
         }
         
         int phrase_length = end - start - 1;
@@ -569,10 +588,11 @@ std::string checksumPort(int sock, sockaddr_in server_addr, int port, const char
 void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in server_addr, std::string secret_phrase, const char *target_ip){
 
     char message[BUFFER_SIZE];
-    std::vector<int> secret_ports;
+    std::vector<int> secret_ports; // vector with order of knocks
     snprintf(message, sizeof(message), "%d,%d", secret_port1, secret_port2); // combine secret ports
     std::cerr << "Sending: " << message << " to port: " << port << std::endl;
 
+    // getting the message with the order of knocks
     for (int attempt = 0; attempt < RETRY_COUNT; ++attempt){
 
         server_addr.sin_port = htons(port);
@@ -602,7 +622,7 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
         }
 
         // getting the ports in order of knocks
-        // splitting the buffer
+        // splitting the buffer with a separator
         char separator = ',';
         int i = 0;
         std::string s;
@@ -612,12 +632,12 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
         buffer[recv_len + 1] = '\0';
         while (buffer[i] != '\0') {
             if (buffer[i] != separator) {
-                // Append the char to the temp string.
+                // append the char to the temp string.
                 s += buffer[i];
             } else {
                 //std::cout << s << std::endl;
                 int t = stoi(s, 0, 10);
-                // Check if the port number is expected one
+                // check if the port number is expected one
                 assert(t == secret_port1 || t == secret_port2);
                 //std::cout << t << std::endl;
                 secret_ports.push_back(t);
@@ -631,6 +651,8 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
         std::cout << "knock: " << getKnock(secret_phrase) << std::endl;
   
         int j = 0;
+
+        // sending knocks in given order
         for(int porty : secret_ports){
             j++;
 
@@ -652,7 +674,7 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
             {
                 std::cerr << "failed to set socket timeout: " << strerror(errno) << std::endl;
                 close(sock);
-                continue;
+                return;
             }
 
             sockaddr_in server_addr2;
@@ -663,19 +685,22 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
             {
                 std::cerr << "given IP address is weird" << std::endl;
                 close(sock);
-                continue;
+                return;
             }
 
             if (sendto(socky, knock, (4 + secret_phrase.length()), 0, (struct sockaddr *)&server_addr2, sizeof(server_addr2)) < 0)
             {
                 perror("sendto failed\n");
+                return;
             }
             else
             {
                std::cout << j << " knock sent!" << std::endl;
             }
+
+            // if it's the last we get the response
             if(j == secret_ports.size()){
-                std::cout << "last port" << std::endl;
+                //std::cout << "last port" << std::endl;
                 char buffer[BUFFER_SIZE] = {0};
                 sockaddr_in response_addr;
                 socklen_t addr_len = sizeof(response_addr);
@@ -696,6 +721,84 @@ void expstn(int port, int secret_port1, int secret_port2, int sock, sockaddr_in 
     }
 }
 
+// tried method for bonus, unfortuanlety did not manage
+/*
+void ICMP(const char *target_ip, int port, sockaddr_in server_addr) {
+    // Create a raw socket to receive ICMP packets
+    int sd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sd < 0) {
+        perror("socket() error");
+        return;
+    }
+    std::cout << "OK: Raw ICMP socket created.\n";
+
+    struct timeval timeout;
+    timeout.tv_sec = 5; // 5 seconds
+    timeout.tv_usec = 0; // 0 microseconds
+    if (setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt() error");
+        close(sd);
+        return;
+    }
+    std::cout << "OK: Receive timeout set to 5 seconds.\n";
+
+    // Bind the socket to the target IP address
+    if (bind(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind() error");
+        close(sd);
+        return;
+    }
+    std::cout << "OK: Bound to target IP: " << target_ip << "\n";
+
+    std::cout << "Listening for ICMP packets...\n";
+
+    char buffer[BUFFER_SIZE];
+    sockaddr_in response_addr;
+    socklen_t addr_len = sizeof(response_addr);
+
+    int recv_len = recvfrom(sd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&response_addr, &addr_len);
+    if (recv_len < 0) {
+        perror("recvfrom() error");
+        close(sd);
+        return;
+    }
+
+    struct iphdr* iph = (struct iphdr*)buffer;
+    unsigned int ip_header_length = iph->ihl * 4;
+
+    std::cout << "IP Total Length: " << ntohs(iph->tot_len) << " bytes\n";
+
+    if (recv_len < ip_header_length + sizeof(struct icmphdr)) {
+        std::cerr << "Received packet is too short for ICMP.\n";
+        close(sd);
+        return;
+    }
+
+    struct icmphdr* icmp_header = (struct icmphdr*)(buffer + ip_header_length);
+
+    std::cout << "ICMP Type: " << static_cast<int>(icmp_header->type) << std::endl;
+    std::cout << "ICMP Code: " << static_cast<int>(icmp_header->code) << std::endl;
+    std::cout << "ICMP Checksum: " << ntohs(icmp_header->checksum) << std::endl;
+
+    char* icmp_payload = buffer + ip_header_length + sizeof(struct icmphdr);
+    ssize_t icmp_payload_len = recv_len - ip_header_length - sizeof(struct icmphdr);
+
+    std::cout << "ICMP Payload (" << icmp_payload_len << " bytes): ";
+    std::cout << "ICMP Payload: ";
+    for (ssize_t i = 0; i < icmp_payload_len; i++) {
+        if (isprint(static_cast<unsigned char>(icmp_payload[i]))) {
+            std::cout << icmp_payload[i];
+        } else {
+            std::cout << ".";
+        }
+    }
+    std::cout << std::endl;
+
+    close(sd);
+}
+*/
+
+// MAIN
 int main(int argc, char *argv[])
 {
 
@@ -732,7 +835,7 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        // Set a receive timeout of 0.5 seconds
+        // set a receive timeout of 0.5 seconds
         struct timeval timeout;
         timeout.tv_sec = TIMEOUT_SEC;
         timeout.tv_usec = TIMEOUT_USEC;
@@ -754,6 +857,7 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // because of UDP packets we usually try to send it couple times if it doesn't work
         for (int attempt = 0; attempt < RETRY_COUNT; ++attempt)
         {
 
@@ -790,11 +894,11 @@ int main(int argc, char *argv[])
                 }
 
                 // E.X.P.S.T.N
-
                 if (buffer_str.find("Greetings! I am E.X.P.S.T.N, ") != std::string::npos)
                 {
                     std::cout << "expstn" << std::endl;
                     expstn(port, secret_port1, secret_port2, sock, server_addr, secret_phrase, target_ip);
+                    //ICMP(target_ip, port, server_addr);
                 }
 
                 // checksum port
